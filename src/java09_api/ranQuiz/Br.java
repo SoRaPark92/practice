740827fec209837fe6d04e6b0088bb36a021de7e
SoRaPark92 <riiiak@naver.com>
2023-07-14 17:12:13 +0900

    20230714 시험 다음주 금요일 자료구조책...??

--- src/java09_api/ranQuiz/Br.java
+++ src/java09_api/ranQuiz/Br.java
@@ -4,62 +4,94 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Br {
-	public static Random ran = new Random();	//랜덤 객체 생성
-	public static Scanner sc= new Scanner(System.in);
-
+	
 	public static void main(String[] args) {
-
-		boolean turn = ran.nextBoolean();	//0: 컴퓨터 , 1 : 사용자
-		int currentCount = 0;
-		while(currentCount<31) {
-			if (turn) {
-				System.out.println("user 의 차례입니다");
-				currentCount = userTurn(currentCount);
-			} else {
-				System.out.println("com 의 차례입니다");
-				currentCount = comTurn(currentCount);
-			}
-			turn = !turn;
-		}
-		
-		
-
-	}
+	int com;//컴퓨터의 숫자(랜덤)
+	int user;//사용자의 숫자(키보드 입력)
 	
-	/**
-	 * 사용자 차례일때 처리하는 로직
-	 * @param currentCount
-	 * @return
-	 */
-	private static int userTurn(int currentCount) {
-		System.out.println("1-3숫자를입력해주세요");
-		int	input = sc.nextInt();
-		for(int i=0; i< input; i++) {
-			if(currentCount>=31) {
-				System.out.println("user 패배입니다");
-				break;
-			}
-			System.out.println(++currentCount);
-		}
-		return currentCount;
-	}
+	Random ran = new Random();//랜덤 객체
+	Scanner sc = new Scanner(System.in);//키보드 입력 객체
 	
-	private static int comTurn(int currentCount) {
-		int com = ran.nextInt(3)+1 ;
-		for(int i=0; i< com; i++) {
-			if (i != 0 && currentCount==30) {
-				System.out.println("com은 30이 가까워져서 도망갑니다.");
-				break;
-			}
+	int num = 1;	//게임 진행 숫자
+	int max = 31;	//게임 종료 숫자(정상)
+//	int max = 11;	//게임 종료 숫자(테스트)
+	
+	//승자 체크 변수
+	// -> 1: user승
+	// -> 2: com승
+	int winner = 0;
+	
+	final int USER_WIN = 1;
+	final int COM_WIN = 2;
+	
+	GAME:
+	while(num<=max) {
+
+		//-----  컴퓨터의 숫자 말하기 ------
+		
+		//컴퓨터의 보정값
+		//	-> 랜덤을 무시하고 컴퓨터가 이길 수 있는 연속 숫자까지만 출력함
+		
+		if( max-num >= 1 && max-num <= 3 ) {
+			com = max-num;
+		
+//			System.out.println("[TEST] com 보정됨 : " + com);
+		
+		} else {
+			com = ran.nextInt(3)+1;	//컴퓨터가 출력할 연속 숫자의 개수, 1~3
+			//com = 3;	//테스트
 			
-			if(currentCount>=31) {
-				System.out.println("com 패배입니다");
-				break;
-			}
-			System.out.println(++currentCount);
+//			System.out.println("[TEST] com 랜덤 : " + com);
 		}
-		return currentCount;
+		
+		com = ran.nextInt(3) + 1;	//컴퓨터가 출력할 연속 숫자의 개수, 1~3
+//		System.out.println("[TEST] com : " + com);
+		
+		for(int i=0; i<com; i++) {
+			System.out.println("com >> " + num++);	
+			
+			if(num>max) {
+				winner = USER_WIN;//사용자의 승리 체크
+				break GAME;
+			}
+		}
+		
+		
+		//-----  사용자의 숫자 말하기 ------
+		
+		//1~3 입력할 때까지 반복
+		while(true) {
+			System.out.println();
+			System.out.println(">> 몇 개 연속? ");
+			user= sc.nextInt();
+			
+			if( user >= 1 && user <=3 ) {
+				break;
+			} else {
+				System.out.println("[SYSTEM] 1~3만 입력하세요");
+			}
+		}
+		
+		for(int i=0; i<user; i++) {
+			System.out.println("user >> " + num++);	
+		
+			if(num>max) {
+				winner = COM_WIN;//컴퓨터의 승리 체크
+				break GAME;
+			}
+		}
+	}//while end, 게임 진행 반복문(max까지)
+	//승자 출력
+	System.out.println();
+	if( winner == USER_WIN ) {
+		System.out.println(">> 사용자가 승리하였습니다");
+		
+	} else if( winner == COM_WIN ) {
+		System.out.println(">> 컴퓨터가 승리하였습니다");
+		
 	}
+	
+}
 }
 //+ 배스킨라빈스31 게임
 //- 컴퓨터와 사용자가 번갈아가면서 연속적으로 숫자를 말한다
